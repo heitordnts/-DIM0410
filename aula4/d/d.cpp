569e98ef056e23e01e6ec1a72d7803040ebd3da2
HeitorDantas <heitor.dantas17@gmail.com>
2019-03-14 16:12:48 -0300

    d

---
+++ aula4/d/d.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main(){
+	stack<int> pilha;
+	vector<int> line;
+	int N;
+
+	while(cin >> N, N > 0){
+		int c;
+		for(int i=0;i<N;i++){
+			if(cin >> c, c == 0) break;
+			//else
+			line.push_back(c);
+		}
+		//verificar permutacao
+		int j=1;
+		for(int i=0;i<N;i++){
+			int atual = line[i];
+			
+			for(;j<=N;j++){
+				pilha.push(j);
+				if(j==atual){
+					pilha.pop();
+					break;
+				}
+			}
+
+		}
+		//--
+		line.clear();
+	}
+
+
+	return 0;
+}
