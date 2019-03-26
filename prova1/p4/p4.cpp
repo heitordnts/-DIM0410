d9d6cce9552369adbfd9c961bf8792a0d0b61e5b
HeitorDantas <heitor.dantas17@gmail.com>
2019-03-25 22:55:22 -0300

    prova

---
+++ prova1/p4/p4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main(){
+	/*
+	forma de guarda os dados:
+		guardar cada conjunto de problemas de cada amigo, como um 'set'.
+		para cada elemento de cada set procuramos se ele existe nos outros dois
+		se existir em algum ,ele nao sera acrescentadoa lista do amigo correspondente
+		verificamos qual a maior lista de problemas only e imprimimos quem e quais
+	*/
+	int n;	
+	cin >> n;
+
+	return 0;
+}
