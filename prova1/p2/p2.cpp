198e956809386824ab1e48338914f70edc76b86f
HeitorDantas <heitor.dantas17@gmail.com>
2019-03-27 21:39:04 -0300

    p2

--- prova1/p2/p2.cpp
+++ prova1/p2/p2.cpp
@@ -1,1 +1,24 @@
 #include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	map<vector<int>,int > froshs;
+	int n, course, f=0;
+
+	while(cin >> n, n != 0){
+		for(int i=0;i<n;i++){
+			vector<int> temp;
+			for(int j=0;j<5;j++){
+				cin >> course;
+				temp.push_back(course);
+			}
+			sort(temp.begin(), temp.end());
+			froshs[temp] += 1;
+			temp.clear();
+		}
+		cout << max_element(froshs.begin(), froshs.end())->second << endl;
+
+	}
+	return 0;
+}
