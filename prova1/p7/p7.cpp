15adce84f27ae3d7f3143168e5e00c9c498f8f54
heitordnts <heitor.dantas17@gmail.com>
2019-03-28 16:45:15 -0300

    p7 in structure

---
+++ prova1/p7/p7.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int N,C,w,p;
+	string s,term,text;
+
+	cin >> N;
+
+	while(0 < N--){
+		cin >> C;
+		for(int i=0;i<C;i++){
+			cin >> s >> w >> p;
+			for(int j=0;j<w;j++){
+				cin >> term;
+			}
+
+		}
+		//problem description
+		while(getline(cin, text), text!=""){
+			//process line
+		}
+	}
+
+
+
+	return 0;
+}
