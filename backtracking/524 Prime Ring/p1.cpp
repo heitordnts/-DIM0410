b57070bc7582a16150a77a8cc71eb3a9876aada7
HeitorDantas <heitor.dantas17@gmail.com>
2019-04-25 20:48:47 -0300

    524

--- backtracking/524 Prime Ring/p1.cpp
+++ backtracking/524 Prime Ring/p1.cpp
@@ -2,21 +2,29 @@
 
 using namespace std;
 
-int v[16]={1};
-bool mark[16]={1};;
+int v[128]={1};
+bool mark[128]={1};;
 
 bool isPrime(int n){
-	for(int i=2; i<= n/2; i++){
+	for(int i=2; i <= n/2; i++){
 		if(n%i == 0) return false;
 	}
 	return true;
 }
 
 int backtracking(int n,int j){	
+	if(j == n){
+		if(isPrime(v[0] + v[j-1])){
+			for(int i=0;i<n;i++)
+				cout << v[i] << " ";
+			cout << endl;
+		}
+		return 0;
 
+	}
 	for(int i=2;i<=n;i++){
 		if(mark[i] == 0){
-			if(isPrime(i+j)){
+			if(isPrime(i+v[j-1])){
 				v[j] = i;
 				mark[i] = 1;
 				backtracking(n, j+1);	
@@ -30,11 +38,12 @@ int backtracking(int n,int j){
 
 int main(){
 
-	int n;
+	int n,c=1;
 
 	while(cin>>n){
-		//backtracking();
-
+		cout << "Case " << c++ << ":\n";
+		backtracking(n,1);
+		cout << endl;
 	}
 
 	return 0;
