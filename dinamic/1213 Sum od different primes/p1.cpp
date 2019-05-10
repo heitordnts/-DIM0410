ed286e39a0dab0b8fe3528786537190d95aec754
heitordnts <heitor.dantas17@gmail.com>
2019-05-10 18:39:08 -0300

    aaa

---
+++ dinamic/1213 Sum od different primes/p1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int f(int n, int k){
+	
+}
+
+
+int main(){
+	int n,k;	
+	
+	while(cin>>n>>k, n!=0 || k!=0){
+		cout << f(n,k) << endl;		
+
+	}
+
+	return 0;
+}
