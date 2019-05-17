55a5bad40cad883a01f1eb9252183d9708aab640
heitordnts <heitor.dantas17@gmail.com>
2019-05-17 18:19:34 -0300

    graphs

--- dinamic/1213 Sum od different primes/p1.cpp
+++ dinamic/1213 Sum od different primes/p1.cpp
@@ -1,7 +1,13 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+/*
+f(24,3,y) {2,3,19} {2,5,17}
+a partir de y 
+f(24-x,2,x+1)
+x = 2
 
+*/
 int f(int n, int k){
 	
 }
---
+++ graphs/10067/p.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+#define N 100
+
+
+int G[N][N];
+int forbidden[10000][4];
+
+
+
+void printS(int *state){
+	for(int i=0;i<4;i++)
+		cout << state[i] << " ";
+	cout << endl;
+}
+
+int main(){
+	int origin[4],target[4];
+	int t, n_fbd;
+	cin >> t;
+	while(t--){
+		for(int i=0;i<4;i++)
+			cin >> origin[i];
+		for(int i=0;i<4;i++)
+			cin >> target[i];
+
+		cin >> n_fbd;
+		
+		for(int i=0;i<n_fbd;i++){
+			for(int j=0;j<4;j++){
+				cin >> forbidden[i][j]; }
+		}	
+
+		for(int j=0;j<n_fbd;j++){
+			printS(forbidden[j]);
+		}
+	}
+
+	
+	return 0;
+}
+
