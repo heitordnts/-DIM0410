f2bbcce769dac64aadd6d366de1d6715c30bb53d
HeitorDantas <heitor.dantas17@gmail.com>
2019-05-17 22:28:20 -0300

    825

--- prova2/825 walking on the safe side/p5.cpp
+++ prova2/825 walking on the safe side/p5.cpp
@@ -3,16 +3,28 @@
 
 using namespace std;
 
-int memo[1000][1000];
+int memo[20000][20000];
+
+int bt(int x, int y){
+	if(x < 0 || y < 0) return 0;
+	if(memo[x][y] != -1 || memo[x][y] == 0){
+		return memo[x][y];
+	}
+	else{
+		memo[x][y] = bt(x-1,y) + bt(x,y-1);
+		return memo[x][y];
+	}
+}
 
 int main(){
 	stringstream ss;
 	string s;
-	memset(memo,-1,1000*1000);
-	memo[0][0] = 1;
 	int t,r,c,l,aux;
 	cin >> t;
+	cin.ignore();
 	while(t--){
+		memset(memo,-1,1000*1000);
+		memo[0][0] = 1;
 		cin >> r >> c;
 		cin.ignore();
 		for(int i=0;i<r;i++){
@@ -26,15 +38,17 @@ int main(){
 				//cout << ss.str() << endl;
 				memo[l-1][aux-1]=0;
 			}
-		ss.clear();
-		}
-
-
-		for(int i=0;i<r;i++){
-			for(int j=0;j<c;j++)
-				cout << memo[i][j] << " ";
-			cout << endl;
-		}
+			ss.clear();
+		}//end of input
+		cin.ignore();
+		cout << bt(r-1, c-1) << endl;
+		if(t!=0) cout << endl;	
+//		for(int i=0;i<r;i++){
+//			for(int j=0;j<c;j++){
+//				cout << memo[i][j] << " " ;
+//			}
+//			cout << endl;
+//		}
 	}
 
 
