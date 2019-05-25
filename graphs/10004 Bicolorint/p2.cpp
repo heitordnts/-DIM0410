bf5bc2dfac0facf0816cad893fa23a1902cd988d
HeitorDantas <heitor.dantas17@gmail.com>
2019-05-25 15:23:35 -0300

    g

--- graphs/10004 Bicolorint/p2.cpp
+++ graphs/10004 Bicolorint/p2.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <cstring>
-
+#include <queue>
 
 using namespace std;
 
-int visitados[200];
-enum Cores {SEMCOR, PRETO, BRANCO};
 #define COR(X) (X==1 ? "PRETO" : (X==2 ? "BRANCO" : "SEMCOR"))
+#define OUT(X) (X==1 ? "BICOLORABLE." : "NOT BICOLORABLE.") 
+
+enum Cores {SEMCOR, PRETO, BRANCO};
+
 struct Node{
 	int temAresta;
 	Cores cor=SEMCOR;
 };
+
+int visitados[200];
+queue<int> q;
 
 void printG(Node G[][200],int n){
 	for(int i=0;i<n;i++){
@@ -23,6 +28,7 @@ void printG(Node G[][200],int n){
 
 void colore(Node G[][200], int n, int no,Cores c){
 	for(int i=0;i<n;i++){
+		
 		if(c == PRETO)
 			G[no][i].cor = BRANCO;
 		else if(c == BRANCO)
@@ -30,28 +36,51 @@ void colore(Node G[][200], int n, int no,Cores c){
 	}
 }
 
+int bfs(Node G[][200], int n,int orig){
+	//inicia a fila com a orig
+	int temp;	
+		
+	q.push(orig);
+	while(!q.empty()){
+		temp = q.front();
+		q.pop();
+		if(visitados[temp] == 1){
+			continue;
+		}
+		else{
+			visitados[temp] = 1;
+		}
+				
+		//cout << "No " << temp << endl;
+		//cout << "Cor " << COR(G[temp][0].cor) << endl;
+		//cout << "queue size: " << q.size()<< endl;
 
-int dfs(Node G[][200], int n,int orig){
-	cout <<"No: " << orig << endl; 
-	cout <<"Cor: " << COR(G[orig][0].cor) << endl; 
-	for(int i=0;i< n;i++){
-		if(G[orig][i].temAresta && visitados[i] == 0){
-			cout << orig << " -> " << i << endl;
-			visitados[i] = 1;
-			if(G[i][0].cor == SEMCOR){
-				colore(G,n,i,G[orig][0].cor);	
+		//if(visitados[temp] == 1) continue;
+		for(int i=0;i<n;i++){
+			if(G[temp][i].temAresta == 1 ){
+
+				if(G[i][0].cor == SEMCOR){
+					colore(G,n,i,G[temp][0].cor);
+				}
+				else if(G[temp][0].cor == G[i][0].cor){
+					return 0;
+				}
+				if(visitados[i] == 0){
+					q.push(i);
+				}
 			}
-			//printG(G,n);
-			dfs(G,n,i);
 		}
 	}
+	return 1;
 }
+
 int main(){
 	int n,l;
 	Node G[200][200];
 	int e1,e2;
 	
 	while(cin >> n, n!=0){
+		q = queue<int>();
 		memset(G,0,200*200);
 		memset(visitados,0,200);
 		cin >> l;
@@ -62,9 +91,11 @@ int main(){
 		}
 		for (int i=0;i<n;i++)
 			G[e1][i].cor = BRANCO;
-		visitados[e1] = 1;
-		printG(G,n);
-		dfs(G,n,e1);
+
+		//visitados[e1] = 1;
+		//printG(G,n);
+		cout << OUT(bfs(G,n,e1)) << endl;
+
 	}
 	return 0;
 }
--- graphs/336/p1.cpp
+++ graphs/336/p1.cpp
@@ -13,10 +13,10 @@ struct Node{
 };
 
 unordered_set<int> visitados;
-queue<int> q;
 
-int bfs(map<int,list<int>> &G, map<int,int> &ttls, int orig){
+int bfs(map<int,list<int>> &G, map<int,int> &nivel, int orig,int ttl){
 	//inicia a fila com a orig
+	queue<int> q;
 	int temp;	
 	int cont =0;	
 	q.push(orig);
@@ -30,54 +30,54 @@ int bfs(map<int,list<int>> &G, map<int,int> &ttls, int orig){
 			visitados.insert(temp);
 		}
 				
-		cout << "No " << temp << endl;
-		cout << "ttl " << ttls[temp] << endl;
-		//cout << "queue size: " << q.size()<< endl;
-		if(ttls[temp] < 0) cont++;
-		//if(visitados[temp] == 1) continue;
+		//if(nivel[temp] > ttl) cont++;
 		for(auto &No : G[temp]){
 			if(visitados.find(No) == visitados.end()){
-				ttls[No] = ttls[temp] - 1;
+				if(nivel[No] == 0)
+					nivel[No] = nivel[temp]+1;
 				q.push(No);	
-			}
-			else{
-				int aux = ttls[temp] - 1;
-				if(ttls[No] < aux && aux >= 0){
-					visitados.erase(No);
-					q.push(No);
-				}
 			}
 		}
 	}
-	return cont;
+	for(auto &a: nivel){
+		//cout << a.first << " " << a.second << endl;
+		if(a.second > ttl || a.second ==0 )
+			cont++;
+	}
+	return cont-1;
 }
 
 int main(){
 	int nc;
 	int e1,e2;
 	map<int,list<int> > G;
-	map<int,int> ttls;
+	map<int,int> nivel;
 	int casos=1;	
 	Node aux,aux2;
 	while(cin >> nc, nc!=0){
 		G.clear();
-		q = queue<int>();
-		visitados.clear();
 		
 		for (int i=0;i<nc;i++){
 			cin >> e1 >> e2;
+
+			nivel[e2] = 0;
+			nivel[e1] = 0;
 			G[e1].push_back(e2);
 			G[e2].push_back(e1);
 		}
 		while(1){	
-			ttls.clear();
+			visitados.clear();
 			cin >> e1 >> e2;
-			ttls[e1] = e2;
+			nivel[e1] = 0;
 			if(e1 == 0 && e2 == 0) break;	
 
 			cout << "Case "<<casos++<<": ";
-			cout << bfs(G,ttls,e1) << " nodes not reachable from node " << e1 << " with TTL = " << e2 << "." << endl;
+			cout << bfs(G,nivel,e1,e2) << " nodes not reachable from node " << e1 << " with TTL = " << e2 << "." << endl;
 
+			nivel.clear();
+			for(auto &a: G){
+				nivel[a.first] =  0;
+			}
 		}
 		cin.ignore();
 	}
